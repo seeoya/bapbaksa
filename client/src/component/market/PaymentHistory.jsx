d0e50c3428b1d8fb95037ece38a9cc6973c9e5c2
choiheebeom <appel4192@naver.com>
2024-04-26 17:50:30 +0900

    구매내역 jsx 생성

---
+++ client/src/component/market/PaymentHistory.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PaymentHistory = () => {
+    return(
+        
+        <div id="payment_history">
+
+            
+
+        </div>
+
+    );
+}
+
+export default PaymentHistory;

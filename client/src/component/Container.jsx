fe7b430c31c4472f1e6daba503bd32b60461bbe4
seeoya <seeoya@naver.com>
2024-04-26 17:59:47 +0900

    #11 레이아웃 수정

--- client/src/component/Container.jsx
+++ client/src/component/Container.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import StyleGuide from "./StyleGuide";
+import Copy from "./Copy";
 import Error from "./Error";
 import Home from "./Home";
-import Copy from "./Copy";
+import StyleGuide from "./StyleGuide";
+import ListView from "./market/ListView";
+import MartketLayout from "./market/MarketLayout";
+import MarketList from "./market/MarketList";
+import Payment from "./market/Payment";
+import ShoppingCart from "./market/ShoppingCart";
 
 const Container = () => {
-    return(
-            <div className="container">
-                <Routes>
-                    <Route path="/" element={<div><Home/></div>} />
-                    <Route path="/user" element={<div>USER</div>} />
-                    <Route path="/market" element={<div>MARKET</div>} />
-                    {/* STYLE GUIDE */}
-                    <Route path="/styleguide" element={<StyleGuide />}></Route>
-                    {/* Error */}
-                    <Route path="*" element={<Error />}></Route>
-                    <Route path="/copy" element={<Copy />}></Route>
-                </Routes>
-            </div>
+    return (
+        <div className="container">
+            <Routes>
+                <Route path="/" element={<div><Home /></div>} />
+                <Route path="/user" element={<div>USER</div>} />
+                <Route path="/market" element={<MartketLayout />}>
+                    <Route path="" element={<div>Market</div>}></Route>
+                    <Route path="list" element={<MarketList />}></Route>
+                    <Route path="view/:no" element={<ListView />}></Route>
+                    <Route path="payment" element={<Payment />}></Route>
+                    <Route path="cart" element={<ShoppingCart />}></Route>
+                </Route>
+
+                {/* STYLE GUIDE */}
+                <Route path="/styleguide" element={<StyleGuide />}></Route>
+                {/* Error */}
+                <Route path="/copy" element={<Copy />}></Route>
+                <Route path="*" element={<Error />}></Route>
+            </Routes>
+        </div>
 
 
     );

6304e776fba88b017002c1416e477092098370b9
choiheebeom <appel4192@naver.com>
2024-04-29 10:59:38 +0900

    #17 Copy Layout으로 market layout 수정

--- client/src/component/include/Nav.jsx
+++ client/src/component/include/Nav.jsx
@@ -9,6 +9,11 @@ const Nav = () => {
                 <Link to="/recipe/list" className="link">레시피</Link>
                 <Link to="/styleguide" className="link">StyleGuide</Link>
                 <Link to="/market" className="link_market">Market 가기</Link>
+                <Link to="/market/list" className="link_market">리스트 가기</Link>
+                <Link to="/market/view/:no" className="link_market">제품 상세 페이지 가기</Link>
+                <Link to="/market/payment" className="link_market">결제 페이지 가기</Link>
+                <Link to="/market/cart" className="link_market">장바구니 가기</Link>
+                <Link to="/market/pay-history" className="link_market">결제 내역 가기</Link>
             </div>
         </nav>
 

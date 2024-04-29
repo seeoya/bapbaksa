f256c2a05f6f79d8465f9781f348108ee154b480
post50 <ppm4ppm4@gmail.com>
2024-04-29 16:59:27 +0900

    passport, upload 설정 1차

--- server/main.js
+++ server/main.js
@@ -6,10 +6,43 @@ const path = require("path");
 const session = require("express-session");
 const MemoryStore = require("memorystore")(session);
 const bcrypt = require("bcrypt");
+const pp = require("./lib/passport/passport");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static('/home/ubuntu/user/upload/profile_imgs/'));
+app.use(express.static('C:\\bapbaksa\\upload\\profile_imgs\\'));
+
+// CORS START
+app.use(cors({
+    //  origin: 'http://localhost:3000',
+      origin: 'http://54.206.156.100:3000',
+      credentials: true,
+  }));
+  // CORS END  
+
+// session setting START
+const maxAge = 1000 * 60 * 30;
+const sessionObj = {
+    secret: 'BAPBAKSA',
+    resave: false,
+    saveUninitialized: true,
+    store: new MemoryStore({checkPeriod: maxAge}),
+    cookie: {
+        maxAge: maxAge,
+    }
+};
+app.use(session(sessionObj));
+// session setting END
+
+// passportjs session START
+let passport = pp.passport(app);
+app.post('user/signin_confirm', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/user/signin_form?errMsg=INCORRECT USER ID OR PW',
+}));
+// passportjs session END 
 
 app.get("/", (req, res) => {
     res.send("SERVICE SERVER 3001");
---
+++ server/uploadFile/uploadFile.js
@@ -0,0 +1,39 @@
+const multer = require("multer");
+const fs = require('fs');
+const uuid4 = require("uuid4");
+const path = require('path');
+
+const upload = {
+
+    UPLOAD_MIDDLEWARE : () => {
+
+        const upload = multer({
+            storage: multer.diskStorage({
+                destination(req, file, done) {
+
+                    //let fileDir = `/home/ubuntu/user/upload/profile_imgs/${req.body.u_id}/`;
+                    let fileDir = `C:\\bapbaksa\\upload\\profile_imgs\\${req.body.u_id}\\`;
+                    if (!fs.existsSync(fileDir)) {
+                        fs.mkdirSync(fileDir, {recursive: true});
+                    }
+
+                    done(null, fileDir);
+                },
+                filename(req, file, done) {
+                    let uuid = uuid4();
+                    let extName = path.extname(file.originalname);
+                    let fileName = uuid + extName;
+
+                    done(null, fileName);
+
+                }
+            }),
+            limits: {
+                fileSize: 1024 * 1024,
+            },
+        });
+
+        return upload.single('profile_thum');
+    },
+}
+module.exports = upload;

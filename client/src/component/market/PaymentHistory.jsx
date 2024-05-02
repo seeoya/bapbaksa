a356b4b7f725dd5b2f8f5b83d9280865bde6d3b2
choiheebeom <appel4192@naver.com>
2024-05-02 12:57:14 +0900

    #17 마켓 css 완료

--- client/src/component/market/PaymentHistory.jsx
+++ client/src/component/market/PaymentHistory.jsx
@@ -24,60 +24,79 @@ const PaymentHistory = () => {
     };
 
     return (
-    <div id="shpping_cart_wrap">
-        <div className="title-wrap">
-            결제 내역
-        </div>
 
-        <div className="ingredient-cart-wrap">
-            <div className="ingredient-payment-history">
-                <p>주문 번호: 1</p>
-                <p>주문 시간: 2024.04.29.12:30</p>
-            </div>
-            <div className="select-btn-order-info">
-                <div className="all-select-btn">
-                    <p>전체 선택</p>
-                    <input
-                        className="ingredient-cart-all-check-btn"
-                        type="checkbox"
-                        checked={selectAll}
-                        onChange={toggleSelectAll}
-                    />
+        <div className='content-wrap' id="payment_history_wrap">
+                <h2 className='title'>결제 내역</h2>
+                <div id="payment_total_wrap">
+                    <div className='content ingredient-cart-wrap'>
+                        <div className="ingredient-payment-history">
+                            <div>
+                                <p>주문 번호: 1</p>
+                            </div>
+                            <div>
+                                <p>주문 시간: 2024.04.29.12:30</p>
+                            </div>
+                        </div>
+                        <div className="select-btn-order-info">
+                            <div className="payment-history-btn-status">
+                                <div className="all-select-btn">
+                                    <input
+                                        className="ingredient-cart-all-check-btn"
+                                        type="checkbox"
+                                        checked={selectAll}
+                                        onChange={toggleSelectAll}
+                                        />
+                                        <p>전체 선택</p>
+                                </div>
+                                <p>주문 상태 : 배송 중</p>
+                            </div>
+                        </div>
+                        <div className="ingredient-cart-item">
+                            <div className="payment-history-check">
+                                <input
+                                    type="checkbox"
+                                    checked={selectedItems.includes(0)}
+                                    onChange={() => toggleItemSelection(0)}
+                                />
+                                <img className="ingredient-cart-img" src="/img/방울토마토.jpg" alt="ingredient" />
+                            </div>
+                            <div>
+                                <span>이름:토마토</span>
+                            </div>
+                            <div>
+                                <span>수량:5kg</span>
+                            </div>
+                            <div>
+                                <span>가격:55,000원</span>
+                            </div>
+                        </div>
+                        <div className="ingredient-cart-item">
+                            <div className="payment-history-check">
+                                <input
+                                    type="checkbox"
+                                    checked={selectedItems.includes(1)}
+                                    onChange={() => toggleItemSelection(1)}
+                                />
+                                <img className="ingredient-cart-img" src="/img/상추.jpg" alt="ingredient" />
+                            </div>
+                            <div>
+                                <span>이름:상추</span>
+                            </div>
+                            <div>
+                                <span>수량:5kg</span>
+                            </div>
+                            <div>
+                                <span>가격:80,000원</span>
+                            </div>
+                        </div>
+                    
+                        <div className="ingredient-cart-btn">
+                            <p>가격 : 135,000원</p>
+                            <button>환불 요청</button>
+                        </div>
                 </div>
-                <div>
-                    <p>주문 상태 : 배송 중</p>
-                </div>                
-            </div>
-            <div className="ingredient-cart-item">
-                <input
-                    type="checkbox"
-                    checked={selectedItems.includes(0)}
-                    onChange={() => toggleItemSelection(0)}
-                />
-                <img className="ingredient-cart-img" src="/img/방울토마토.jpg" alt="ingredient" />
-                <span>이름:토마토</span>
-                <span>수량:5kg</span>
-                <span>가격:55,000원</span>
-            </div>
-            <div className="ingredient-cart-item">
-                <input
-                    type="checkbox"
-                    checked={selectedItems.includes(1)}
-                    onChange={() => toggleItemSelection(1)}
-                />
-                <img className="ingredient-cart-img" src="/img/상추.jpg" alt="ingredient" />
-                <span>이름:상추</span>
-                <span>수량:5kg</span>
-                <span>가격:80,000원</span>
             </div>
         </div>
-        <div className="ingredient-cart-btn">
-            <input type="text" placeholder="총 가격" value={"135,000원"}/>
-            <button>가격 : 135,000원</button>
-            <button>선택 취소</button>
-        </div>
-
-    </div>
     )
 }
 

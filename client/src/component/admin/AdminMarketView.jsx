319b53fbc082bdc28d5275e733eb4fd02856504b
post50 <ppm4ppm4@gmail.com>
2024-05-20 17:24:08 +0900

    axios 추가

--- client/src/component/admin/AdminMarketView.jsx
+++ client/src/component/admin/AdminMarketView.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { setTitle } from '../../util/setTitle';
+import axios from 'axios';
 
 const AdminMarketView = () => {
     const { id } = useParams();

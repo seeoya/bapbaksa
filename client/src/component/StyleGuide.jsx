bbf8b92c73ed7c7814ecdc76fd7892863b1d7c00
seeoya <seeoya@naver.com>
2024-05-21 15:28:57 +0900

    스타일 수정중

--- client/src/component/StyleGuide.jsx
+++ client/src/component/StyleGuide.jsx
@@ -1,82 +1,84 @@
 import React from "react";
 
-function StyleGuide () {
+function StyleGuide() {
     return (<>
-        
 
-    <div id="style_guide" className="content-wrap">
-        <h2 className="title">STYLE GUIDE</h2>
 
-        <div className="content">
-            <div>
-                <div className="title">COLOR</div>
-
-                <div className="box-wrap">
-                    <div className="main">
-                        <div className="box light">MAIN - LIGHT</div>                                                                
-                        <div className="box">MAIN</div>                        
-                        <div className="box dark">MAIN - DARK</div>                        
-                    </div>                
-
-                    <div className="sub">                      
-                        <div className="box light">SUB - LIGHT</div>
-                        <div className="box">SUB</div>
-                        <div className="box dark">SUB - DARK</div>
-                    </div>
-
-                    <div className="highlight">
-                        <div className="box light">HIGHLIGHT - LIGHT</div>
-                        <div className="box">HIGHLIGHT</div>                       
-                        <div className="box dark">HIGHLIGHT - DARK</div>
-                        <div className="box red">HIGHLIGHT - RED</div>    
-                    </div>    
-                                            
-                    <div className="gray">
-                        <div className="box light">GRAY - LIGHT</div>
-                        <div className="box">GRAY</div>
-                        <div className="box dark">GRAY - DARK</div>                        
-                        <div className="box green">GRAY - GREEN</div>                        
-                    </div>
-                </div>
-            </div>    
-        </div>    
-
-        <div className="content">
-            <div className="title">FORM(.form)</div>
+        <div id="style_guide" className="content-wrap">
+            <h2 className="title">STYLE GUIDE</h2>
 
             <div className="content">
-                <div className="">
-                    <form action="" className="form">
-                        <input type="text" placeholder="아이디를 입력하세요."/>
-                        <input type="password" placeholder="비밀번호를 입력하세요."/>
-                        <select name="" id="">
-                            <option value="">기본값</option>
-                            <option value="">값1</option>
-                            <option value="">값2</option>
-                        </select>
+                <div>
+                    <div className="title">COLOR</div>
 
-                        <button type="button" className="btn">btn</button>
-                        <button type="button" className="btn main">btn main</button>
-                        <button type="button" className="btn main bold">btn main bold</button>
-                        <button type="button" className="btn sub">btn sub</button>
-                        <button type="button" className="btn highlight">btn highlight</button>
-
-                        <p>.btn-wrap.row</p>
-                        <div className="btn-wrap row">
-                            <button type="button" className="btn">btn</button>
-                            <button type="button" className="btn">btn</button>
+                    <div className="box-wrap">
+                        <div className="main">
+                            <div className="box light">MAIN - LIGHT</div>
+                            <div className="box">MAIN</div>
+                            <div className="box dark">MAIN - DARK</div>
                         </div>
-                        
-                        <p>버튼 너비</p>    
-                        <button type="button" className="btn half">btn half 50%</button>
-                        <button type="button" className="btn over">btn over 75%</button>
-                        <button type="button" className="btn full">btn full 100%</button>
-                    </form>
+
+                        <div className="sub">
+                            <div className="box light">SUB - LIGHT</div>
+                            <div className="box">SUB</div>
+                            <div className="box dark">SUB - DARK</div>
+                        </div>
+
+                        <div className="highlight">
+                            <div className="box light">HIGHLIGHT - LIGHT</div>
+                            <div className="box">HIGHLIGHT</div>
+                            <div className="box dark">HIGHLIGHT - DARK</div>
+                            <div className="box red">HIGHLIGHT - RED</div>
+                        </div>
+
+                        <div className="gray">
+                            <div className="box light">GRAY - LIGHT</div>
+                            <div className="box middle">GRAY - MIDDLE</div>
+                            <div className="box">GRAY</div>
+                            <div className="box deep">GRAY - DEEP</div>
+                            <div className="box dark">GRAY - DARK</div>
+                            <div className="box green">GRAY - GREEN</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="content">
+                <div className="title">FORM(.form)</div>
+
+                <div className="content">
+                    <div className="">
+                        <form action="" className="form">
+                            <input type="text" placeholder="아이디를 입력하세요." />
+                            <input type="password" placeholder="비밀번호를 입력하세요." />
+                            <select name="" id="">
+                                <option value="">기본값</option>
+                                <option value="">값1</option>
+                                <option value="">값2</option>
+                            </select>
+
+                            <button type="button" className="btn">btn</button>
+                            <button type="button" className="btn main">btn main</button>
+                            <button type="button" className="btn main bold">btn main bold</button>
+                            <button type="button" className="btn sub">btn sub</button>
+                            <button type="button" className="btn highlight">btn highlight</button>
+
+                            <p>.btn-wrap.row</p>
+                            <div className="btn-wrap row">
+                                <button type="button" className="btn">btn</button>
+                                <button type="button" className="btn">btn</button>
+                            </div>
+
+                            <p>버튼 너비</p>
+                            <button type="button" className="btn half">btn half 50%</button>
+                            <button type="button" className="btn over">btn over 75%</button>
+                            <button type="button" className="btn full">btn full 100%</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>    
-</>
+    </>
     );
 }
 
--- client/src/component/include/Nav.jsx
+++ client/src/component/include/Nav.jsx
@@ -7,14 +7,12 @@ const Nav = () => {
     const [loginedID, setLoginedID] = useState('');
 
     useEffect(() => {
-
         let loginedUId = getToken('loginedUId');
         console.log('loginedUIdString', loginedUId);
 
         if (loginedUId !== null) {
             setLoginedID(loginedUId);
         }
-
     }, [loginedID]);
 
 
@@ -22,31 +20,31 @@ const Nav = () => {
         <nav>
             <div id="main-category-wrap" className="category-wrap">
                 <div className="header-category">
-                    <div className="recipe">
-                        <Link to="/recipe/list" className="link">레시피 리스트</Link>
-                        <Link to="/recipe/view/1" className="link">레시피 상세</Link>
+                    <div className="main">
+                        <Link to="/recipe/list" className="link">레시피 모두보기</Link>
+                        <Link to="/market" className="link link_market">마켓 보러가기</Link>
+                    </div>
+
+                    <div className="sub">
                         <Link to="/mypage/myfridge" className="link">내 냉장고</Link>
-                        <Link to="/styleguide" className="link">StyleGuide</Link>
+                        <Link to="/mypage/like" className="link">좋아하는 레시피</Link>
                     </div>
-                    <div className="market">
-                        <Link to="/market" className="link link_market">마켓 가기</Link>
-                    </div>
-                    { loginedID !== '' ?
-                    <>
-                    <div className="user-mypage-link">                          
-                        <Link to="/mypage" className="link_mypage" title="마이페이지 바로가기">
-                            <div className="user-info">
-                                <span> {loginedID} </span>
-                                <span> 님 </span>                        
-                                <img src="/imgs/logo/logo.png" alt="마이페이지"/>
-                                {/*<img src={process.env.REACT_APP_SERVER_URL + `/home/ubuntu/user/upload/profile_imgs/${uId}/${uProfile}`} alt="" />*/}
-                            </div>            
-                        </Link>                        
-                    </div>
-                    </>
-                    :
-                    <>
-                    </>
+                    {loginedID !== '' ?
+                        <>
+                            <div className="user-mypage-link">
+                                <Link to="/mypage" className="link_mypage" title="마이페이지 바로가기">
+                                    <div className="user-info">
+                                        <span> {loginedID} </span>
+                                        <span> 님 </span>
+                                        <img src="/imgs/logo/logo.png" alt="마이페이지" />
+                                        {/*<img src={process.env.REACT_APP_SERVER_URL + `/home/ubuntu/user/upload/profile_imgs/${uId}/${uProfile}`} alt="" />*/}
+                                    </div>
+                                </Link>
+                            </div>
+                        </>
+                        :
+                        <>
+                        </>
 
                     }
                 </div>

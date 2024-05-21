728be64e147f301d4d15e5e0bcc5243c583eb5c8
Choi <chm9656@gmail.com>
2024-05-21 10:54:38 +0900

    DB local -> aws RDS로 수정

--- server/lib/db/db.js
+++ server/lib/db/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
 
 const DB = mysql.createConnection({
-    //host: "bapbaksa-db.c1qgim0ec6mg.ap-southeast-2.rds.amazonaws.com",
-    host: "localhost",
+    host: "bapbaksa-db.c1qgim0ec6mg.ap-southeast-2.rds.amazonaws.com",
+    // host: "localhost",
     port: "3306",
     user: "root",
-    //password: "baksa!1234",
-    password: "1234",
+    password: "baksa!1234",
+    // password: "1234",
     database: "DB_BAPBAKSA",
     dateStrings: true,
 });

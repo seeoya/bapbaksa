f465164403f153de5aae2961152d3b47ff02462e
seeoya <seeoya@naver.com>
2024-05-21 15:51:57 +0900

    네비게이션 수정중

--- client/src/component/include/Nav.jsx
+++ client/src/component/include/Nav.jsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getToken } from "../../storage/loginedToken";
@@ -21,13 +22,23 @@ const Nav = () => {
             <div id="main-category-wrap" className="category-wrap">
                 <div className="header-category">
                     <div className="main">
-                        <Link to="/recipe/list" className="link">레시피 모두보기</Link>
-                        <Link to="/market" className="link link_market">마켓 보러가기</Link>
+                        <Link to="/recipe/list" className="link">
+                            <FontAwesomeIcon icon="fa-solid fa-scroll" />
+                            <span>레시피 모두보기</span>
+                        </Link>
+                        <Link to="/market" className="link link_market">
+                            <FontAwesomeIcon icon="fa-solid fa-store" />
+                            <span>마켓 보러가기</span>
+                        </Link>
                     </div>
 
                     <div className="sub">
-                        <Link to="/mypage/myfridge" className="link">내 냉장고</Link>
-                        <Link to="/mypage/like" className="link">좋아하는 레시피</Link>
+                        <Link to="/mypage/myfridge" className="link">
+
+                            <span>내 냉장고</span></Link>
+                        <Link to="/mypage/like" className="link">
+                            <FontAwesomeIcon icon="fa-solid fa-bookmark" />
+                            <span>좋아하는 레시피</span></Link>
                     </div>
                     {loginedID !== '' ?
                         <>
